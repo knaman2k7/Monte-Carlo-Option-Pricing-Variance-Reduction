ab42db8fb8abf9d3c2efc4983eb88b6b21041337
Naman <knaman240@gmail.com>
2026-09-03 23:19:31 +0100

    seperated calls and puts

--- blackScholes.py
+++ blackScholes.py
@@ -2,12 +2,22 @@ import math
 import numpy as np
 from scipy.stats import norm
 
-# calculates an option's price using black scholes equation
-def calculateOptionPrice(S, K, r, t, sigma):
+# calculates a call option's price using black scholes equation
+def calculateCallPrice(S, K, r, t, sigma):
 
     d1 = ( math.log(S/K) + (r + sigma**2 / 2) * t ) / ( sigma * math.sqrt(t) )
     d2 = d1 - sigma * math.sqrt(t)
 
     C = S * norm.cdf(d1) - K * math.exp(-r*t) * norm.cdf(d2)
+
+    return C
+
+# calculates a put option's price using black scholes equation
+def calculatePutPrice(S, K, r, t, sigma):
+
+    d1 = ( math.log(S/K) + (r + sigma**2 / 2) * t ) / ( sigma * math.sqrt(t) )
+    d2 = d1 - sigma * math.sqrt(t)
+
+    C = K * math.exp(-r*t) * norm.cdf(-d2) - S * norm.cdf(-d1)
 
     return C
